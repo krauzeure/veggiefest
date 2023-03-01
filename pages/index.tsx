daf5926842f670d277258cf6c26212c41ba47de1
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-01 23:11:43 +0100

    Adding homepage images and testing layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import Head from 'next/head';
 import styles from '@/pages/index.module.css';
+import Image from 'next/image';
+import image1 from '../assets/anna-pelzer-IGfIGP5ONV0-unsplash.jpg';
+import image2 from '../assets/joseph-gonzalez-fdlZBWIP0aM-unsplash.jpg';
+import image3 from '../assets/joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg'
 
 export default function Home() {
   return (
@@ -14,10 +18,20 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>Hello world</h1>
+        <section className={styles.headSection}>
+          <div>
+            <h1 className={styles.title}>Bienvenue sur Veggiefest !</h1>
+            <h2 className={styles.subtitle}>
+              Le site de recettes dédié aux plats végétariens
+            </h2>
+          </div>
+          <div className={styles.image}>
+            <Image src={image1} alt='Image de nourriture' width={300} height={300} />
+            <Image src={image2} alt='Image de nourriture' width={250} height={250} />
+            <Image src={image3} alt='Image de nourriture' width={350} height={350} />
+          </div>
+        </section>
       </main>
-
-      <footer className={styles.footer}></footer>
     </>
   );
 }
