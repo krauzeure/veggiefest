01e2b71e936e18ce0a35df1c95c0f44fccf1a6cd
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-02 09:47:39 +0100

    Editing homepage style and global font family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import '@/styles/global.css';
 import type { AppProps } from 'next/app';
 import Container from '@/components/Container/Container';
+import { Poppins } from 'next/font/google';
+const poppins = Poppins({ weight: ['400', '600', '700'], subsets:['latin'] })
+
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Container>
+      <style jsx global>{`
+        html {
+          font-family: ${poppins.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
     </Container>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,10 @@ import styles from '@/pages/index.module.css';
 import Image from 'next/image';
 import image1 from '../assets/anna-pelzer-IGfIGP5ONV0-unsplash.jpg';
 import image2 from '../assets/joseph-gonzalez-fdlZBWIP0aM-unsplash.jpg';
-import image3 from '../assets/joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg'
+import image3 from '../assets/joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg';
+
+import { Yeseva_One } from 'next/font/google'
+const yeseva = Yeseva_One({ weight: '400', subsets:['latin'] })
 
 export default function Home() {
   return (
@@ -20,15 +23,31 @@ export default function Home() {
       <main>
         <section className={styles.headSection}>
           <div>
-            <h1 className={styles.title}>Bienvenue sur Veggiefest !</h1>
-            <h2 className={styles.subtitle}>
-              Le site de recettes dédié aux plats végétariens
-            </h2>
+            <h1 className={`${styles.title} ${yeseva.className}`}><span className={styles.titleKeyword}>Veggiefest</span>, le site réservé aux recettes végétariennes</h1>
           </div>
           <div className={styles.image}>
-            <Image src={image1} alt='Image de nourriture' width={300} height={300} />
-            <Image src={image2} alt='Image de nourriture' width={250} height={250} />
-            <Image src={image3} alt='Image de nourriture' width={350} height={350} />
+            <div>
+              <Image
+                src={image1}
+                alt='Image de nourriture'
+                width={400}
+                height={400}
+              />
+            </div>
+            <div>
+              <Image
+                src={image2}
+                alt='Image de nourriture'
+                width={200}
+                height={200}
+              />
+              <Image
+                src={image3}
+                alt='Image de nourriture'
+                width={150}
+                height={150}
+              />
+            </div>
           </div>
         </section>
       </main>
