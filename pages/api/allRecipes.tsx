eac941632d936672a407fe2b1ec5f0c9f0c4d518
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-03 12:31:51 +0100

    Adding filters on difficulty, time and type

--- pages/api/allRecipes.tsx
+++ pages/api/allRecipes.tsx
@@ -1,7 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { recipes } from 'data/recipes'
+import { recipe } from 'types/types';
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json({ recipes: recipes })
+    const filters = req.query.difficulty;
+    let filteredRecipes: recipe[];
+    if(req.query.difficulty) {
+        filteredRecipes = recipes.filter(el => el.difficulty === Number(req.query.difficulty))
+    } else {
+        filteredRecipes = recipes
+    }
+    if(req.query.time) {
+        filteredRecipes = filteredRecipes.filter(el => el.time === Number(req.query.time))
+    }
+    if(req.query.recipeType) {
+        filteredRecipes = filteredRecipes.filter(el => el.recipeType.toLowerCase() === req.query.recipeType)
+    }
+    res.status(200).json({ recipes: filteredRecipes, queries: filters })
 }
