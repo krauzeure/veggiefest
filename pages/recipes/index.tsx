9f99d323428e507cc6c583593ab48adee3e7caf9
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-03 16:40:24 +0100

    Moving to SSR for data fetching

--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -1,34 +1,30 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import { recipe } from 'types/types';
 import Recipe from '@/components/Recipe/Recipe';
 
-import styles from './index.module.css'
+import styles from './index.module.css';
 
-export default function Recipes() {
-  const [recipes, setRecipes] = useState<recipe[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const getRecipes = async () => {
-    const res = await fetch(`/api/allRecipes`);
-    const data = await res.json();
-    setRecipes(data.recipes);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    getRecipes();
-  }, []);
-
+export default function Recipes({ recipes }: { recipes: [] }) {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>Recettes</h1>
-      {!isLoading && (
-        <ul>
-          {recipes.map((item: recipe, index: number) => (
-            <Recipe key={index} name={item.name} image={item.image}/>
-          ))}
-        </ul>
-      )}
+      <ul>
+        {recipes.map((item: recipe, index: number) => (
+          <Recipe key={index} name={item.name} image={item.image} />
+        ))}
+      </ul>
     </main>
   );
+}
+
+export async function getServerSideProps() {
+  const data = await fetch('http://localhost:3000/api/allRecipes');
+  const result = await data.json();
+  const recipes = result.recipes;
+
+  return {
+    props: {
+      recipes,
+    },
+  };
 }
