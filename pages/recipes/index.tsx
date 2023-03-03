2a8f3f94641b7718e533578ca920b9e78d312040
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-03 12:36:30 +0100

    Styling the recipes title

--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import { recipe } from 'types/types';
 
+import styles from './index.module.css'
+
 export default function Recipes() {
   const [recipes, setRecipes] = useState<recipe[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const getRecipes = async () => {
-    const res = await fetch(`/api/allRecipes`);
+    const res = await fetch(`/api/allRecipes?time=60&difficulty=1&recipeType=plat`);
     const data = await res.json();
     setRecipes(data.recipes);
     setIsLoading(false);
@@ -17,8 +19,8 @@ export default function Recipes() {
   }, []);
 
   return (
-    <>
-      <h1>Recipes</h1>
+    <main className={styles.main}>
+      <h1 className={styles.title}>Recettes</h1>
       {!isLoading && (
         <ul>
           {recipes.map((item: recipe, index: number) => (
@@ -26,6 +28,6 @@ export default function Recipes() {
           ))}
         </ul>
       )}
-    </>
+    </main>
   );
 }
