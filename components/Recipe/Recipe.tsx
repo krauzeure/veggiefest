db93b34d390501945c1faae8e3e311a037fdd6af
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-03 15:47:06 +0100

    Creating the recipe component and styling it

---
+++ components/Recipe/Recipe.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './Recipe.module.css'
+import Image, { StaticImageData } from 'next/image'
+
+export default function Recipe(props: {name: string, image: StaticImageData}) {
+  return (
+    <li className={styles.container}><Image src={props.image} alt={props.name} width={250} height={150}/><p>{props.name}</p></li>
+  )
+}
