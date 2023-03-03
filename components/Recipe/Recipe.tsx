af3c8f94ce64dde4a82dc2f872f8899d1c89e6ab
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-03 18:54:00 +0100

    Editing recipe card style

--- components/Recipe/Recipe.tsx
+++ components/Recipe/Recipe.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import styles from './Recipe.module.css'
-import Image, { StaticImageData } from 'next/image'
+import React from 'react';
+import styles from './Recipe.module.css';
+import Image, { StaticImageData } from 'next/image';
 
-export default function Recipe(props: {name: string, image: StaticImageData}) {
+export default function Recipe(props: {
+  name: string;
+  image: StaticImageData;
+}) {
   return (
-    <li className={styles.container}><Image src={props.image} alt={props.name} width={250} height={150}/><p>{props.name}</p></li>
-  )
+    <li className={styles.container}>
+      <Image src={props.image} alt={props.name} width={250} height={150} />
+      <p>{props.name}</p>
+    </li>
+  );
 }
