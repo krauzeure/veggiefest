bd3ae4cba562892515048a5448846d2434b86aae
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-03 10:54:43 +0100

    Storing our recipes in the state and mapping on the results to test

--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { recipe } from 'types/types';
 
 export default function Recipes() {
+  const [recipes, setRecipes] = useState<recipe[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const getRecipes = async () => {
     const res = await fetch(`/api/allRecipes`);
     const data = await res.json();
-    console.log(data);
+    setRecipes(data.recipes);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     getRecipes();
   }, []);
 
-  return <div>Recipes</div>;
+  return (
+    <>
+      <h1>Recipes</h1>
+      {!isLoading && (
+        <ul>
+          {recipes.map((item: recipe, index: number) => (
+            <li key={index}>{item.name}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
