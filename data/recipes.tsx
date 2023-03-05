dc62ebb1418f7042616af7e455014ebcdde931c0
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-05 21:03:07 +0100

    Adding recipes

--- data/recipes.tsx
+++ data/recipes.tsx
@@ -4,6 +4,13 @@ import Gaufres from '../assets/10404_w1280h972c1cx1000cy1500cxb2000cyb3000.webp'
 import Tatin from '../assets/86217_w1280h972c1cx3050cy4137cxb4337cyb6500.webp'
 import Spaghetti from '../assets/61540_w1280h972c1cx1500cy996.webp'
 import Omelette from '../assets/125932_w1280h972c1cx954cy709cxb2121cyb1414.webp'
+import OeufCoque from '../assets/135678_w2000h3000c1cx695cy1193cxb1414cyb1918.webp'
+import OeufMimosa from '../assets/17648_w2000h2904c1cx1328cy1928.webp'
+import Souffle from '../assets/61662_w2000h3000cx1000cy1500.webp'
+import TarteOignon from '../assets/80679_w2000h2667c1cx1944cy2592cxb3888cyb5184.webp'
+import GateauChocolat from '../assets/93106_w2000h1124c1cx3744cy2104cxb7488cyb4208.webp'
+import Crepes from '../assets/124598_w2000h1333c1cx3176cy2107cxt1161cyt477cxb5347cyb3565.webp'
+import TartePomme from '../assets/128250_w2000h1445c1cx1294cy688cxb2037cyb1472.webp'
 
 import { StaticImageData } from 'next/image'
 
@@ -61,5 +68,68 @@ export const recipes: {name: string, image: StaticImageData, difficulty: number,
     ingredients: [{qty: "1", name: "Oignon"}, {qty: "4", name: "Oeufs"}, {qty: "250g", name: "Épinards en branche"}, {qty: "50g", name: "Mozzarella"}],
     steps: ["Emincer l'oignon et sa verdure.", "Battre les oeufs avec les épinards (cuits à l'avance), saler et poivrer.", "Fondre une noisette de beurre dans une poêle et faire revenir l'oignon 2 mn, puis ajouter le mélange oeufs/épinards. Saupoudrer de mozzarella (râpée ou tranchée finement).", "Laisser cuire à feu doux, jusqu'à ce que les oeufs soient cuits et la mozzarella fondue."],
     recipeType: "Plat"
+  },
+  {
+    name: "Oeufs à la coque",
+    image: OeufCoque,
+    difficulty: 1,
+    time: 4,
+    ingredients: [{qty: "6", name: "Oeufs"}, {qty: "1", name: "Baguette"}],
+    steps: ["Choisissez des oeufs extra frais et de taille similaire pour une cuisson homogène Sortez-les du réfrigérateur 1 heure avant de les cuire pour qu’ils soient à température ambiante", "Mettre de l'eau à chauffer Lorsque l'eau bout, plonger les oeufs dedans avec une cuillère et compter 3 minutes de cuisson avant de les sortir de l'eau", "Coupez la baguette en mouillettes", "Ouvrez le haut de l'oeuf, trempez les mouilettes et dégustez !"],
+    recipeType: "Entrée"
+  },
+  {
+    name: "Oeufs mimosa",
+    image: OeufMimosa,
+    difficulty: 1,
+    time: 35,
+    ingredients: [{qty: "4", name: "Oeufs"}, {qty: "1", name: "Baguette"}],
+    steps: ["Faire cuire les oeufs 10 mn dans l'eau bouillante, puis les mettre dans l'eau froide pour arrêter la cuisson.", "Ecaler les oeufs, les couper dans le sens de la longueur, puis séparer les blancs des jaunes.", "Dans une assiette creuse, émietter les jaunes à la fourchette, mélanger la moitié de ces jaunes émiettés avec de la mayonnaise et réserver le reste.", "Remplir les demi-blancs de cette préparation, puis saupoudrer chaque demi-oeuf du reste de jaunes émiettés."],
+    recipeType: "Entrée"
+  },
+  {
+    name: "Soufflé au fromage",
+    image: Souffle,
+    difficulty: 1,
+    time: 50,
+    ingredients: [{qty: "150g", name: "Emmental rapé"}, {qty: "60g", name: "Beurre"}, {qty: "60g", name: "Farine"}, {qty: "4", name: "Oeufs"}, {qty: "40cl", name: "Lait"}],
+    steps: ["Préchauffer le four à 180°C (thermostat 6). Beurrer le moule.", "Chauffer le beurre dans une casserole, ajouter la farine et remuer rapidement pendant 1 min. Ajouter le lait tiédi, remuer au fouet pendant quelques minutes à feu doux.", "Retirer la casserole du feu.", "Séparer les blancs et les battre fermement (avec une pincée de sel).", "Dans la casserole refroidie, ajouter les jaunes d'oeufs un à un, puis le fromage râpé.", "Incorporer les blancs d'oeufs battus en mélangeant délicatement.", "Verser dans le moule, au maximum jusqu'à 4 cm du bord.", "Enfourner pendant 35 minutes en position chaleur tournante."],
+    recipeType: "Entrée"
+  },
+  {
+    name: "Tarte à l'oignon rapide",
+    image: TarteOignon,
+    difficulty: 1,
+    time: 45,
+    ingredients: [{qty: "1", name: "Pâte brisée"}, {qty: "5", name: "Oignons"}, {qty: "20cl", name: "Crême fraiche"}, {qty: "2", name: "Oeufs"}],
+    steps: ["Faire suer les oignons émincés finement dans une poêle avec de l'huile, saler et poivrer.", "Battre les oeufs avec la crème fraîche.", "Lorsque les oignons ont blondi, ajouter hors du feu le mélange oeufs/crème.", "Verser le tout sur la pâte brisée étalée dans un moule, faire cuire 30 min au four à 200°C (thermostat 6-7)."],
+    recipeType: "Entrée"
+  },
+  {
+    name: "Gâteau au chocolat fondant",
+    image: GateauChocolat,
+    difficulty: 1,
+    time: 40,
+    ingredients: [{qty: "200g", name: "Chocolat patissier"}, {qty: "100g", name: "Beurre"}, {qty: "50g", name: "Farine"}, {qty: "100g", name: "Sucre en poudre"}, {qty: "3", name: "Oeufs"}],
+    steps: ["Préchauffez votre four à 180°C (thermostat 6). Dans une casserole, faites fondre le chocolat et le beurre coupé en morceaux à feu très doux.", "Dans un saladier, ajoutez le sucre, les oeufs, la farine. Mélangez.", "Ajoutez le mélange chocolat/beurre. Mélangez bien.", "Beurrez à l'aide d'une feuille de papier essuie-tout et farinez votre moule puis y versez la pâte à gâteau.", "Faites cuire au four environ 20 minutes.", "A la sortie du four le gâteau ne paraît pas assez cuit. C'est normal, laissez-le refroidir puis démoulez- le."],
+    recipeType: "Dessert"
+  },
+  {
+    name: "Pâte à crèpes",
+    image: Crepes,
+    difficulty: 1,
+    time: 70,
+    ingredients: [{qty: "300g", name: "Farine"}, {qty: "3 càs", name: "Sucre"}, {qty: "2 càs", name: "Huile"}, {qty: "50g", name: "Beurre fondu"}, {qty: "5cl", name: "Rhum"}, {qty: "3", name: "Oeufs"}, {qty: "60cl", name: "Lait"}],
+    steps: ["Mettre la farine dans un saladier et former un puits.", "Y déposer les oeufs entiers, le sucre, l'huile et le beurre.", "Mélanger délicatement avec un fouet en ajoutant au fur et à mesure le lait. La pâte ainsi obtenue doit avoir une consistance d'un liquide légèrement épais.", "Parfumer de rhum.", "Faire chauffer une poêle antiadhésive et la huiler très légèrement à l'aide d'un papier Essuie-tout. Y verser une louche de pâte, la répartir dans la poêle puis attendre qu'elle soit cuite d'un côté avant de la retourner. Cuire ainsi toutes les crêpes à feu doux."],
+    recipeType: "Dessert"
+  },
+  {
+    name: "Tarte aux pommes",
+    image: TartePomme,
+    difficulty: 1,
+    time: 55,
+    ingredients: [{qty: "1", name: "Pâte brisée"}, {qty: "1 sachet", name: "Sucre vanillée"}, {qty: "30g", name: "Beurre"}, {qty: "6", name: "Pommes golden"}],
+    steps: ["Éplucher et découper en morceaux 4 Golden.", "Faire une compote : les mettre dans une casserole avec un peu d'eau (1 verre ou 2). Bien remuer. Quand les pommes commencent à ramollir, ajouter un sachet ou un sachet et demi de sucre vanillé. Ajouter un peu d'eau si nécessaire.", "Vous saurez si la compote est prête une fois que les pommes ne seront plus dures du tout. Ce n'est pas grave s'il reste quelques morceaux.", "Pendant que la compote cuit, éplucher et couper en quatre les deux dernières pommes, puis, couper les quartiers en fines lamelles (elles serviront à être posées sur la compote).", "Préchauffer le four à 210°C (thermostat 7).", "Laisser un peu refroidir la compote et étaler la pâte brisée dans un moule et la piquer avec une fourchette.", "Verser la compote sur la pâte et placer les lamelles de pommes en formant une spirale ou plusieurs cercles, au choix ! Disposer des lamelles de beurre dessus.", "Mettre au four et laisser cuire pendant 30 min max. Surveiller la cuisson. Vous pouvez ajouter un peu de sucre vanillé sur la tarte pendant que çà cuit pour caraméliser un peu."],
+    recipeType: "Dessert"
   }
 ]
