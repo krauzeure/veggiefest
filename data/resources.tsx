79af66186ae8d314a13b9e05de4fa56cc234a84c
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-07 22:10:11 +0100

    Adding the resources data

---
+++ data/resources.tsx
@@ -0,0 +1,72 @@
+export const recipesResources = [
+  {
+    name: 'Dahl de lentilles corail',
+    link: 'https://www.marmiton.org/recettes/recette_dahl-de-lentilles-corail_166862.aspx',
+  },
+  {
+    name: 'Quiche façon croque-monsieur',
+    link: 'https://www.marmiton.org/recettes/recette_quiche-facon-croque-monsieur_345346.aspx',
+  },
+  {
+    name: 'Gaufres salées au pesto',
+    link: 'https://www.marmiton.org/recettes/recette_gaufres-salees-au-pesto_68832.aspx',
+  },
+  {
+    name: 'Tarte Tatin aux oignons rouges',
+    link: 'https://www.marmiton.org/recettes/recette_tarte-tatin-aux-oignons-rouges_33809.aspx',
+  },
+  {
+    name: 'Spaghetti aux courgettes',
+    link: 'https://www.marmiton.org/recettes/recette_spaghetti-aux-courgettes_10951.aspx',
+  },
+  {
+    name: 'Omelette aux épinards et mozzarella',
+    link: 'https://www.marmiton.org/recettes/recette_omelette-aux-epinards-et-mozzarella_30264.aspx',
+  },
+  {
+    name: 'Oeufs à la coque',
+    link: 'https://www.marmiton.org/recettes/recette_conseils-pour-reussir-vos-oeufs-a-la-coque_28560.aspx',
+  },
+  {
+    name: 'Oeufs mimosa',
+    link: 'https://www.marmiton.org/recettes/recette_oeufs-mimosa_28602.aspx',
+  },
+  {
+    name: 'Soufflé au fromage',
+    link: 'https://www.marmiton.org/recettes/recette_souffle-au-fromage_69641.aspx',
+  },
+  {
+    name: 'Tarte à l’oignon',
+    link: 'https://www.marmiton.org/recettes/recette_tarte-a-l-oignon-rapide_26201.aspx',
+  },
+  {
+    name: 'Gateau chocolat',
+    link: 'https://www.marmiton.org/recettes/recette_gateau-au-chocolat-fondant-rapide_166352.aspx',
+  },
+  {
+    name: 'Pâte à crêpes',
+    link: 'https://www.marmiton.org/recettes/recette_pate-a-crepes_12372.aspx',
+  },
+  {
+    name: 'Tarte aux pommes',
+    link: 'https://www.marmiton.org/recettes/recette_tarte-aux-pommes_18588.aspx',
+  },
+];
+
+export const photoResources = [
+  {
+    name: 'Photo salade - accueil',
+    link: 'https://unsplash.com/fr/photos/IGfIGP5ONV0',
+    artist: 'Anna Pelzer',
+  },
+  {
+    name: 'Photo sandwich - accueil',
+    link: 'https://unsplash.com/fr/photos/fdlZBWIP0aM',
+    artist: 'Joseph Gonzalez',
+  },
+  {
+    name: 'Photo pain et myrtilles - accueil',
+    link: 'https://unsplash.com/fr/photos/zcUgjyqEwe8',
+    artist: 'Joseph Gonzalez',
+  },
+];
