cce3d610e0ec0de447384c7947a979e2a1e5d30d
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-07 10:59:33 +0100

    changing difficulty level to string

--- types/types.tsx
+++ types/types.tsx
@@ -3,7 +3,7 @@ import { StaticImageData } from "next/image";
 export interface recipe {
     name: string,
     image: StaticImageData,
-    difficulty: number,
+    difficulty: string,
     time: number,
     ingredients: {qty: string, name: string}[],
     steps: string[],
