e654d2606d030e5e9ce44df0b4ce2fa42d172335
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-07 22:10:43 +0100

    Adding types for resources

--- types/types.tsx
+++ types/types.tsx
@@ -1,12 +1,23 @@
-import { StaticImageData } from "next/image";
+import { StaticImageData } from 'next/image';
 
 export interface recipe {
-    name: string,
-    image: StaticImageData,
-    difficulty: string,
-    time: number,
-    ingredients: {qty: string, name: string}[],
-    steps: string[],
-    recipeType: string,
-    id: number
-  }
+  name: string;
+  image: StaticImageData;
+  difficulty: string;
+  time: number;
+  ingredients: { qty: string; name: string }[];
+  steps: string[];
+  recipeType: string;
+  id: number;
+}
+
+export interface recipeResources {
+  name: string;
+  link: string;
+}
+
+export interface photoResources {
+  name: string;
+  link: string;
+  artist: string;
+}
