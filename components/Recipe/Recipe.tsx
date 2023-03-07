07695f52901686f51fdb8d18113e6d356d3ca4ad
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-07 12:00:21 +0100

    Adding link to recipe on recipe card and styling the card

--- components/Recipe/Recipe.tsx
+++ components/Recipe/Recipe.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import styles from './Recipe.module.css';
 import Image, { StaticImageData } from 'next/image';
+import Link from 'next/link';
 
 export default function Recipe(props: {
   name: string;
   image: StaticImageData;
   difficulty: string;
   time: number;
+  id: number;
 }) {
-  
   return (
     <li className={styles.container}>
-      <Image src={props.image} alt={props.name} width={250} height={150} />
-      <div className={styles.info}>
-      <p>{props.name}</p>
-        <div className={styles.difficulty}>{props.difficulty}</div>
-      </div>
+      <Link href={`/recipes/${props.id}`}>
+        <Image src={props.image} alt={props.name} width={250} height={150} />
+        <div className={styles.info}>
+          <p>{props.name}</p>
+          <p className={styles.difficulty}>{props.difficulty}</p>
+        </div>
+      </Link>
     </li>
   );
 }
