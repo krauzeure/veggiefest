36fad4687504f780dc55ec880863d5ce285b8ba1
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-07 11:43:00 +0100

    Switching to string for difficulty and updating the style of the recipe card to reflect this

--- components/Recipe/Recipe.tsx
+++ components/Recipe/Recipe.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import styles from './Recipe.module.css';
 import Image, { StaticImageData } from 'next/image';
-import broccoli from '../../assets/broccoli.png'
-import broccoliFilled from '../../assets/broccoli_filled.png'
 
 export default function Recipe(props: {
   name: string;
   image: StaticImageData;
-  difficulty: number;
+  difficulty: string;
   time: number;
 }) {
-
-    const totalDifficultyRating = 3;
-    const difficultyArr = Array.from(Array(3))
-    difficultyArr.fill(broccoliFilled, 0, props.difficulty).fill(broccoli, props.difficulty, totalDifficultyRating)
+  
   return (
     <li className={styles.container}>
       <Image src={props.image} alt={props.name} width={250} height={150} />
       <div className={styles.info}>
       <p>{props.name}</p>
-        <div className={styles.difficulty}>Difficulté : {difficultyArr.map((item, index) => (<Image src={item} alt="Difficulté" width={15} height={15} key={index} />))}</div>
+        <div className={styles.difficulty}>{props.difficulty}</div>
       </div>
     </li>
   );
