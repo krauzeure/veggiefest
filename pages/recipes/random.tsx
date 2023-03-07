7c475dce0270003f1ece34c0dfe2b577ea5c7359
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-03-07 09:55:46 +0100

    getting the random recipe from the API

--- pages/recipes/random.tsx
+++ pages/recipes/random.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { recipe } from 'types/types'
 
-export default function Random() {
+export default function randomRecipe({recipe, randomNumber}: {recipe: recipe, randomNumber: number}) {
+  console.log(randomNumber)
+  console.log(recipe)
   return (
-    <div>random</div>
+    <div>mh</div>
   )
+}
+
+export async function getServerSideProps() {
+    const data = await fetch("http://localhost:3000/api/randomRecipe")
+    const result = await data.json()
+    return {
+        props: {
+            recipe: result.recipe,
+            randomNumber: result.randomNumber
+        }
+    }
 }
